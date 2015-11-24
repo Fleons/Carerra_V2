6a526e93338928716cb8ee648ee3f231d9d84b72
Fleons <geiger.max@me.com>
2015-11-24 19:24:54 +0100

    ....

--- Arduino/Carrera_V2/SerialMangagement.ino
+++ Arduino/Carrera_V2/SerialMangagement.ino
@@ -1,28 +1,8 @@
 //**************************CARRERA-VERSION 2**********************************
-//*****************************************************************************
-#include <EEPROM.h>
-#define SpeedPin 9                                                              //Steuert Basis vom Transistor
-#define SensorPin 0                                                             //Aktuell Potentiometer, später Handdynamometer
+//**************************SERIAL - MANAGEMENT********************************
 
-word SpeedValue;                                                                //Geschwindigkeit des Fahrzeuges in PWM (0-255)
-word MaxSpeedValue = 255;                                                       //Max. Geschwindigkeit des Fahrzeuges
-word PWM;
-byte VehicleCalibrationValue, SpeedFactor = 1;
-
-
-void setup() {
-  pinMode(SpeedPin, OUTPUT);                                                    //setzen des Ausgangspin für den Transistor
-  Serial.begin(9600);
-
-
-
-}
-
-void loop() {
-  if (Serial.available())
-  {
-      serialIntput(readStringUntil(";"));                                       //Funktion in SerialManagement.ino
-
-  }
+void serialInput(string Parameter_Data)
+{
+    
 
 }
