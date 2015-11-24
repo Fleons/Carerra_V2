bf16def5c9a2f477cea7fb673fc762a9f6fd79e9
Wolfgang Votruba <w.votruba@outlook.at>
2015-11-24 19:22:55 +0100

    add serial string

--- Arduino/Carrera_V2/Carrera_V2.ino
+++ Arduino/Carrera_V2/Carrera_V2.ino
@@ -9,11 +9,19 @@ word MaxSpeedValue = 255;                                                       
 word PWM;
 byte VehicleCalibrationValue, SpeedFactor = 1;
 
+//Serial
+String Data_Phrase = "DATA";
+String Cal_Phrase = "CAL";
+String SFactor_Phrase = "SFACTOR";
+String Control_Phrase = "CONTROL";
+String VMax_Phrase = "VMAX";
+
+
 
 void setup() {
   pinMode(SpeedPin, OUTPUT);                                                    //setzen des Ausgangspin für den Transistor
   Serial.begin(9600);
-
+  VehicleCalibrationValue = EEPROM.read(EEPROM_VehicleCalibrationValue);        //Kalibrierungswert aus dem EEPROM auslesen.
 
 
 }
