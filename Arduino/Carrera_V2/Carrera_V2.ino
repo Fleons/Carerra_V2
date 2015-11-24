267ee2afd286282042899c7142466c0cc01c6569
Wolfgang Votruba <w.votruba@outlook.at>
2015-11-24 19:41:42 +0100

    Drive

--- Arduino/Carrera_V2/Carrera_V2.ino
+++ Arduino/Carrera_V2/Carrera_V2.ino
@@ -3,11 +3,12 @@
 #include <EEPROM.h>
 #define SpeedPin 9                                                              //Steuert Basis vom Transistor
 #define SensorPin 0                                                             //Aktuell Potentiometer, später Handdynamometer
+#define EEPROM_VehicleCalibrationValue 0                                        //Nullpunktdefinition der Geschwindigkeit
 
-word SpeedValue;                                                                //Geschwindigkeit des Fahrzeuges in PWM (0-255)
+word SpeedValue = 0, SignalValue = 0;                                           //Geschwindigkeit des Fahrzeuges in PWM (0-255)
 word MaxSpeedValue = 255;                                                       //Max. Geschwindigkeit des Fahrzeuges
-word PWM;
 byte VehicleCalibrationValue, SpeedFactor = 1;
+bool RunFlag = true;
 
 //Serial
 String Data_Phrase = "DATA";
@@ -16,21 +17,16 @@ String SFactor_Phrase = "SFACTOR";
 String Control_Phrase = "CONTROL";
 String VMax_Phrase = "VMAX";
 
-
-
 void setup() {
   pinMode(SpeedPin, OUTPUT);                                                    //setzen des Ausgangspin für den Transistor
   Serial.begin(9600);
   VehicleCalibrationValue = EEPROM.read(EEPROM_VehicleCalibrationValue);        //Kalibrierungswert aus dem EEPROM auslesen.
-
-
 }
 
 void loop() {
+  drive();
   if (Serial.available())
   {
       serialIntput(readStringUntil(";"));                                       //Funktion in SerialManagement.ino
-
   }
-
 }
--- Arduino/Carrera_V2/Drive.ino
+++ Arduino/Carrera_V2/Drive.ino
@@ -1,28 +1,25 @@
-//**************************CARRERA-VERSION 2**********************************
-//*****************************************************************************
-#include <EEPROM.h>
-#define SpeedPin 9                                                              //Steuert Basis vom Transistor
-#define SensorPin 0                                                             //Aktuell Potentiometer, später Handdynamometer
+void drive() {
+  SpeedValue = analogRead(SensorPin);                                           //Wert des Sensors
+  SignalValue = SpeedValue;
+    if (RunFlag) {                                                              //Sonst "NOTAUS"
 
-word SpeedValue;                                                                //Geschwindigkeit des Fahrzeuges in PWM (0-255)
-word MaxSpeedValue = 255;                                                       //Max. Geschwindigkeit des Fahrzeuges
-word PWM;
-byte VehicleCalibrationValue, SpeedFactor = 1;
+        //Sensorwert durch 4, da PWM nur bis 255 und Sensor bis 1023
+        //SpeedValue hat nun einen Wert im Bereich von 0 bis 255 - dem Kalibrierungswertes
+        SpeedValue = map(SpeedValue/4,0,255,0,255-VehicleCalibrationValue);     //Geschwindikeit ab kalibriertem Nullpunkt                    //map(value, fromLow, fromHigh, toLow, toHigh)
+        if (SpeedValue != 0){                                                   // Keine Bremsung
 
+        //SpeedValue wird jetzt mit einem definierten Faktor multipliziert.
+        //Dadurch kann auch ein Kind den Looping fahren und größere Geschwindigkeiten erreichen.
+        //Zuletzt wird der Wert bei einem max. von 255 (max. PWM) abgeschnitten.
+        // --> SpeedValue max. Wert somit 255
+        SpeedValue = constrain((SpeedFactor * SpeedValue) + VehicleCalibrationValue,0,255);
 
-void setup() {
-  pinMode(SpeedPin, OUTPUT);                                                    //setzen des Ausgangspin für den Transistor
-  Serial.begin(9600);
-
-
-
-}
-
-void loop() {
-  if (Serial.available())
-  {
-      serialIntput(readStringUntil(";"));                                       //Funktion in SerialManagement.ino
-
-  }
-
+        SpeedValue = map(SpeedValue,0,255,0,MaxSpeedValue);                     //Max. Geschwindigkeit begrenzen
+      }
+      else {                                                                    //Bremsung
+        SpeedValue = 0;
+      }
+      analogWrite(SpeedPin, SpeedValue);                                        //Setzen der Geschwindikeit des Fahrzeugs
+    }
+    delay(50);
 }
